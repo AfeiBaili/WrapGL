8e5021fb3b15a6b10c2792b846ff15402837a632
Afei <afeibaili@outlook.com>
2026-06-01 22:04:36 +0800

    build project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    `maven-publish`
 }
 
-group = "cn.afeibaili"
-version = "1.0-SNAPSHOT"
+group = "com.github.afeibaili"
+version = "0.0.1"
+val mavenPackageName = "wrapgl"
+
+val lwjglVersion = "3.3.6"
+val jomlVersion = "1.10.8"
+val lwjglNatives = "natives-windows"
 
 repositories {
     mavenCentral()
@@ -11,11 +17,51 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-freetype")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-freetype", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    implementation("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            groupId = project.group.toString()
+            artifactId = mavenPackageName
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/afeibaili/wrapgl")
+            credentials {
+                username = System.getenv("GITHUB_ACCOUNT")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
 kotlin {
     jvmToolchain(17)
 }
